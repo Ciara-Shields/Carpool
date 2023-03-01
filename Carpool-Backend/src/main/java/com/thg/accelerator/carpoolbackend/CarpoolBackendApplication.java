a6724e719819f545da0af32f6870fb00a7060e01
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-03-01 15:35:09 +0000

    first commit

---
+++ Carpool-Backend/src/main/java/com/thg/accelerator/carpoolbackend/CarpoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thg.accelerator.carpoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolBackendApplication.class, args);
+    }
+
+}
---
+++ Carpool-Backend/src/test/java/com/thg/accelerator/carpoolbackend/CarpoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thg.accelerator.carpoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
